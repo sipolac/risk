29fed6c60c8b63b069af2c8e21af2123166f23fc
Chris Sipola <sipolac@gmail.com>
2019-08-15 14:17:18 -0400

    Major update to model: uses recursive approach. Put old code in graveyard

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
 # risk
+
+This project has some functions for computing the probabilities of outcomes in the board game [Risk](https://en.wikipedia.org/wiki/Risk_(game)).
+
+It uses a recursive approach that's faster and (in my opinion) more elegant than a direct implementation of a [pure statistics approach](http://www4.stat.ncsu.edu/~jaosborn/research/RISK.pdf).
+
+
+# Arguments
+
+See help: `python risk.py -h`
+
+```
+usage: risk.py [-h] [--stop STOP] a d
+
+positional arguments:
+  a            num. troops on attacking territory
+  d            num. troops on defending territory
+
+optional arguments:
+  -h, --help   show this help message and exit
+  --stop STOP  when attack has this many troops or fewer, stop
+```
+
+
+# Examples
+
+To see the probabilities of the outcomes of a battle where the number of troops on the attacking territory is 10 and on the defending territory is 7, then do:
+    `python risk.py 10 5`
+
+This will print:
+```
+(attack, defense): probability
+(1, 1): 0.028092878284246413
+(1, 2): 0.04781649215850439
+(1, 3): 0.029224148925045684
+(1, 4): 0.01646900113237808
+(1, 5): 0.005461006848598092
+(2, 0): 0.020066341631604583
+(3, 0): 0.04306937234319355
+(4, 0): 0.07561101389364897
+(5, 0): 0.10547405944755822
+(6, 0): 0.12157456616254411
+(7, 0): 0.14544059489072236
+(8, 0): 0.1468051595275586
+(9, 0): 0.12376894769401886
+(10, 0): 0.09112641706037806
+
+defense: cumulative probability (at least)
+0: 1.0
+1: 0.12706352734877266
+2: 0.09897064906452625
+3: 0.05115415690602186
+4: 0.021930007980976173
+5: 0.005461006848598092
+
+attack: cumulative probability (at least)
+1: 1.0
+2: 0.8729364726512274
+3: 0.8528701310196228
+4: 0.8098007586764293
+5: 0.7341897447827803
+6: 0.628715685335222
+7: 0.5071411191726779
+8: 0.36170052428195554
+9: 0.2148953647543969
+10: 0.09112641706037806
+
+win prob: 0.8729364726512274
+```
+
+Some conclusions you could make from this:
+* The probability of attack overtaking defense is 87.3% (see last printed statement).
+* The probability of attack having more than, say, 8 troops at the end of the engagement is 36.2%.
+* The probability that the outcome of attack having exactly 8 troops at the end of the engagement is 14.7%.
+
+The code can be used in Python as well:
+```python
+>>> import risk
+>>> risk.calc_battle_probs(10, 7)
+{(10, 0): 0.03386771415952837, (9, 0): 0.06323706306848197, (8, 0): 0.09529206908693112, (7, 0): 0.11386017602833826, (6, 0): 0.11615211502207937, (5, 0): 0.1159717383838905, (4, 0): 0.09843767152386411, (3, 0): 0.060036192808549695, (2, 0): 0.02922503013626597, (1, 1): 0.040915042190772355, (1, 2): 0.07532197109219943, (1, 3): 0.05871256743457267, (1, 4): 0.04781649215850439, (1, 5): 0.029224148925045684, (1, 6): 0.01646900113237808, (1, 7): 0.005461006848598092}
+```
+
+# Dependencies
+None
+
+
+# TODO
+* Generalize for dice with N sides
+* Generalize for multiple defense territories
---
+++ risk/risk.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Authors: Chris Sipola
+Created: 2019-08-15
+
+Functions for computing battle outcome probabilities in the board game Risk.
+"""
+from collections import defaultdict
+from operator import itemgetter
+from typing import Dict, List, Tuple
+import argparse
+
+
+STOP_DEFAULT = 1
+
+
+# Keys are (# attack dice, # defense dice). Values are dicts whose keys are
+# (# attack losses, # defense losses) and values are the probability of that
+# outcome.
+loss_probs = {
+    (3, 2): {
+        (0, 2): 2890 / 7776,
+        (2, 0): 2275 / 7776,
+        (1, 1): 2611 / 7776
+    },
+    (3, 1): {
+        (0, 1): 855 / 1296,
+        (1, 0): 441 / 1296
+    },
+    (2, 2): {
+        (0, 2): 295 / 1296,
+        (2, 0): 581 / 1296,
+        (1, 1): 420 / 1296
+    },
+    (2, 1): {
+        (0, 1): 125 / 216,
+        (1, 0): 91 / 216
+    },
+    (1, 2): {
+        (0, 1): 55 / 216,
+        (1, 0): 161 / 216
+    },
+    (1, 1): {
+        (0, 1): 15 / 36,
+        (1, 0): 21 / 36
+    }
+}
+
+
+def calc_battle_probs(a: int,
+                      d: int,
+                      stop: int = STOP_DEFAULT) -> Dict[Tuple[int], float]:
+    """Get probability of all outcomes.
+
+    Args:
+        a: Number of troops on attacking territory
+        d: Number of troops on defending territory
+        stop: When attack has this many troops or fewer, stop
+
+    Returns:
+        Dict where key is tuple of final attack and defense troops, and
+        values are probabilites of those outcomes
+    """
+    return calc_battle_probs_recur(a, d, stop, 1, dict())
+
+
+def calc_battle_probs_recur(a, d, stop, p: float, memo):
+    """Recursive helper.
+
+    Additional args:
+        p: Probability of seeing current number of attack and defense troops
+        memo: Memo of probabilities where key is tuple of `a` and `d`
+    """
+
+    if a <= stop or d == 0:
+        return {(a, d): 1}  # outcome has prob = 1 in absorbing state
+    if (a, d) in memo:
+        return memo[(a, d)]
+
+    # Compute probabilities of next roll outcomes.
+    next_loss_probs = loss_probs[(min(3, a - 1), min(2, d))]
+    probs_list = list()
+    for (a_loss, d_loss), p2 in next_loss_probs.items():
+        a2, d2 = a - a_loss, d - d_loss  # new attack and defense counts
+        if (a2, d2) in memo:
+            new = memo[(a2, d2)]
+        else:
+            new = calc_battle_probs_recur(a2, d2, stop, p, memo)
+        # Multiply by upstream probability.
+        new = {(a, d): p * p2 for (a, d), p in new.items()}
+        probs_list.append(new)
+
+    combined = combine_dict_probs(probs_list)
+
+    memo[(a, d)] = combined
+    return memo[(a, d)]
+
+def combine_dict_probs(dict_list):
+    """Combine list dicts into single dict, summing probabilies."""
+    combined = defaultdict(int)
+    for d in dict_list:
+        for key, prob in d.items():
+            combined[key] += prob
+    return dict(combined)
+
+
+def calc_win_prob(battle_probs):
+    return sum([p for (a, d), p in battle_probs.items() if d == 0])
+
+
+def calc_cum_probs(battle_probs, attack: bool):
+    """Calculate cumulative probs for attack or defense."""
+    index = 0 if attack else 1
+    battle_prob_list = sorted(battle_probs.items(),
+                              key=lambda tup: tup[0][index],
+                              reverse=True)
+    probs = dict()
+    cum_prob = 0
+    for tup, p in battle_prob_list:
+        cum_prob += p
+        probs[tup[index]] = cum_prob
+    return sorted(probs.items())
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('a', type=int, help='num. troops on attacking territory')
+    parser.add_argument('d', type=int, help='num. troops on defending territory')
+    parser.add_argument('--stop',
+                        default=STOP_DEFAULT,
+                        type=int,
+                        help='when attack has this many troops or fewer, stop')
+    # parser.add_argument('-c',
+    #                     '--cumulative',
+    #                     action='store_true',
+    #                     default=False,
+    #                     help='make battle probabilities cumulative')
+    args = parser.parse_args()
+    assert args.stop > 0
+
+    battle_probs = calc_battle_probs(args.a, args.d, args.stop)
+    win_prob = calc_win_prob(battle_probs)
+
+    # Print all probabilities.
+    print('(attack, defense): probability')
+    battle_prob_list = sorted(battle_probs.items(), key=itemgetter(0))
+    for (a, d), p in battle_prob_list:
+        print(f'({a}, {d}): {p}')
+
+    # Print cumulative probabilities.
+    for attack in [False, True]:
+        print(f'\n{"attack" if attack else "defense"}: cumulative probability (at least)')
+        cum_probs = calc_cum_probs(battle_probs, attack)
+        for num, p in cum_probs:
+            print(f'{num}: {p}')
+
+    print(f'\nwin prob: {win_prob}')
+
+
+if __name__ == '__main__':
+    main()
