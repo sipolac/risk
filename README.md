bd232c955d332a84922ed02bd9f83162ce982a9c
Chris Sipola <sipolac@gmail.com>
2019-08-16 14:31:31 -0400

    Allow for choice of number of sides on attack and defense dice

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 This project has some functions for computing the probabilities of outcomes in the board game [Risk](https://en.wikipedia.org/wiki/Risk_(game)).
 
-It uses a recursive approach that's a bit nicer than a direct implementation of a [pure statistics approach](http://www4.stat.ncsu.edu/~jaosborn/research/RISK.pdf).
+A few advantages of this implementation:
+* The probabilities are exact (it doesn't use simulation).
+* It's very fast for typical inputs.
+* It generalizes to dice with N sides. This is helpful if you're playing on [warfish.net](warfish.net) or another platform that has user-created maps with attack/defense bonuses.
+* Allows user to specify when they want to stop attacking. This is useful if they want to guarantee a certain number of troops on the attacking territory after the engagement---maybe to defend against a different adjacent territory.
+* Under the hood, it's a bit cleaner than the direct implementation of a [Markov-based statistics approach](http://www4.stat.ncsu.edu/~jaosborn/research/RISK.pdf).
+
+Code is written in Python 3.7.4.
 
 
 # Arguments
@@ -10,75 +17,110 @@ It uses a recursive approach that's a bit nicer than a direct implementation of 
 See help: `python risk.py -h`
 
 ```
-usage: risk.py [-h] [--stop STOP] a d
+usage: risk.py [-h] [--asides ASIDES] [--dsides DSIDES] [--stop STOP] a d
 
 positional arguments:
-  a            num. troops on attacking territory
-  d            num. troops on defending territory
+  a                number of troops on attacking territory
+  d                number of troops on defending territory
 
 optional arguments:
-  -h, --help   show this help message and exit
-  --stop STOP  when attack has this many troops or fewer, stop
+  -h, --help       show this help message and exit
+  --asides ASIDES  number of sides on attack dice
+  --dsides DSIDES  number of sides on defense dice
+  --stop STOP      when attack has this many troops or fewer, stop
 ```
 
 
 # Examples
 
+## Command line
+
 For a battle where the attacking territory has 10 and the defending territory has 5:
 
-    python risk.py 10 5
+    python risk.py 5 4
 
 This will print:
 ```
 (attack, defense): probability
-(1, 1): 0.028092878284246413
-(1, 2): 0.04781649215850439
-(1, 3): 0.029224148925045684
-(1, 4): 0.01646900113237808
-(1, 5): 0.005461006848598092
-(2, 0): 0.020066341631604583
-(3, 0): 0.04306937234319355
-(4, 0): 0.07561101389364897
-(5, 0): 0.10547405944755822
-(6, 0): 0.12157456616254411
-(7, 0): 0.14544059489072236
-(8, 0): 0.1468051595275586
-(9, 0): 0.12376894769401886
-(10, 0): 0.09112641706037806
+(1, 1): 0.08263191440573789
+(1, 2): 0.16578464212787658
+(1, 3): 0.1438941163997636
+(1, 4): 0.13115845129680434
+(2, 0): 0.0590227960040985
+(3, 0): 0.11472143265834381
+(4, 0): 0.16465818335269697
+(5, 0): 0.13812846375467833
 
 defense: cumulative probability (at least)
 0: 1.0
-1: 0.12706352734877266
-2: 0.09897064906452625
-3: 0.05115415690602186
-4: 0.021930007980976173
-5: 0.005461006848598092
+1: 0.5234691242301824
+2: 0.4408372098244445
+3: 0.27505256769656794
+4: 0.13115845129680434
 
 attack: cumulative probability (at least)
-1: 1.0
-2: 0.8729364726512274
-3: 0.8528701310196228
-4: 0.8098007586764293
-5: 0.7341897447827803
-6: 0.628715685335222
-7: 0.5071411191726779
-8: 0.36170052428195554
-9: 0.2148953647543969
-10: 0.09112641706037806
+1: 1.0000000000000002
+2: 0.4765308757698176
+3: 0.41750807976571913
+4: 0.3027866471073753
+5: 0.13812846375467833
 
-win prob: 0.8729364726512274
+win prob: 0.47653087576981756
 ```
 
 Some readings of the above output:
-* The probability of attack overtaking defense is 87.3% (see last printed statement).
-* The probability of attack having more than, say, 8 troops at the end of the engagement is 36.2%.
-* The probability that the outcome of attack having exactly 8 troops at the end of the engagement is 14.7%.
+* The probability of attack overtaking defense is 47.7% (see last printed line).
+* The probability of attack having *at least* 3 troops at the end of the engagement is 41.8%.
+* The probability of attack having *exactly* 3 troops at the end of the engagement is 11.5%.
+
+If defense has a +2 bonus and is therefore rolling with 8-sided dice:
+
+    python3 risk.py 10 5 --dsides 8
+
+Which gives you:
+
+```
+(attack, defense): probability
+(1, 1): 0.08373009443903778
+(1, 2): 0.19414771967101685
+(1, 3): 0.2379265900038769
+(1, 4): 0.26489844930516626
+(2, 0): 0.038059133835926254
+(3, 0): 0.06613429494193751
+(4, 0): 0.07139900856820838
+(5, 0): 0.043704709234880686
+
+defense: cumulative probability (at least)
+0: 1.0000000000000508
+1: 0.7807028534190978
+2: 0.6969727589800601
+3: 0.5028250393090432
+4: 0.26489844930516626
+
+attack: cumulative probability (at least)
+1: 1.0000000000000506
+2: 0.21929714658095284
+3: 0.18123801274502657
+4: 0.11510371780308906
+5: 0.043704709234880686
+
+win prob: 0.21929714658095284
+```
+
+You see that attack's win probability drops to 21.9%
+
+## Python
 
 To use it in Python:
 ```python
 >>> import risk
->>> risk.calc_battle_probs(10, 5)
-{(10, 0): 0.09112641706037806, (9, 0): 0.12376894769401886, (8, 0): 0.1468051595275586, (7, 0): 0.14544059489072236, (6, 0): 0.12157456616254411, (5, 0): 0.10547405944755822, (4, 0): 0.07561101389364897, (3, 0): 0.04306937234319355, (2, 0): 0.020066341631604583, (1, 1): 0.028092878284246413, (1, 2): 0.04781649215850439, (1, 3): 0.029224148925045684, (1, 4): 0.01646900113237808, (1, 5): 0.005461006848598092}
+>>> battle_probs = risk.calc_battle_probs(5, 4, d_sides=8)
+>>> battle_probs
+{(1, 4): 0.26489844930516626, (1, 3): 0.2379265900038769, (1, 2): 0.19414771967101685, (1, 1): 0.08373009443903778, (2, 0): 0.038059133835926254, (3, 0): 0.06613429494193751, (4, 0): 0.07139900856820838, (5, 0): 0.043704709234880686}
+>>> risk.calc_win_prob(battle_probs)
+0.21929714658095284
+>>> risk.calc_cum_probs(battle_probs, attack=True)  # for attack's values
+[(1, 1.0000000000000506), (2, 0.21929714658095284), (3, 0.18123801274502657), (4, 0.11510371780308906), (5, 0.043704709234880686)]
 ```
 
 # Dependencies
@@ -86,5 +128,4 @@ None
 
 
 # TODO
-* Generalize for dice with N sides
 * Generalize for multiple defense territories
--- risk/risk.py
+++ risk/risk.py
@@ -7,48 +7,99 @@ Created: 2019-08-15
 Functions for computing battle outcome probabilities in the board game Risk.
 """
 from collections import defaultdict
+from functools import lru_cache
+from itertools import product
 from operator import itemgetter
 import argparse
 
 
+A_SIDES_DEFAULT = 6
+D_SIDES_DEFAULT = 6
 STOP_DEFAULT = 1
 
 
-# Keys are (# attack dice, # defense dice). Values are dicts whose keys are
-# (# attack losses, # defense losses) and values are the probability of that
-# outcome.
-loss_probs = {(3, 2): {(0, 2): 2890 / 7776,
-                       (2, 0): 2275 / 7776,
-                       (1, 1): 2611 / 7776},
-              (3, 1): {(0, 1): 855 / 1296,
-                       (1, 0): 441 / 1296},
-              (2, 2): {(0, 2): 295 / 1296,
-                       (2, 0): 581 / 1296,
-                       (1, 1): 420 / 1296},
-              (2, 1): {(0, 1): 125 / 216,
-                       (1, 0): 91 / 216},
-              (1, 2): {(0, 1): 55 / 216,
-                       (1, 0): 161 / 216},
-              (1, 1): {(0, 1): 15 / 36,
-                       (1, 0): 21 / 36}}
+loss_probs_6_6 = {(3, 2): {(0, 2): 2890 / 7776,
+                           (2, 0): 2275 / 7776,
+                           (1, 1): 2611 / 7776},
+                  (3, 1): {(0, 1): 855 / 1296,
+                           (1, 0): 441 / 1296},
+                  (2, 2): {(0, 2): 295 / 1296,
+                           (2, 0): 581 / 1296,
+                           (1, 1): 420 / 1296},
+                  (2, 1): {(0, 1): 125 / 216,
+                           (1, 0): 91 / 216},
+                  (1, 2): {(0, 1): 55 / 216,
+                           (1, 0): 161 / 216},
+                  (1, 1): {(0, 1): 15 / 36,
+                           (1, 0): 21 / 36}}
 
 
-def calc_battle_probs(a, d, stop=STOP_DEFAULT):
+@lru_cache(maxsize=128)
+def get_loss_probs(a_sides, d_sides):
+    """Returns probability of various loss combinations.
+
+    Args:
+        a_sides: Int for number of sides on attacking dice
+        d_sides: Int for number of sides on defending dice
+
+    Returns:
+        Dict of probailities of loss combinations. Keys are (# attack dice,
+        # defense dice). Values are dicts whose keys are (# attack losses, #
+        # defense losses) and values are the probability of that outcome.
+    """
+    if a_sides == 6 and d_sides == 6:
+        return loss_probs_6_6
+
+    loss_probs = dict()
+    for a_rolls, d_rolls in product(range(1, 4), range(1, 3)):
+        loss_probs_rolls = defaultdict(int)
+
+        # Generate all combos.
+        a_roll_vals = list(range(1, a_sides + 1))
+        d_roll_vals = list(range(1, d_sides + 1))
+        iters = [a_roll_vals] * a_rolls + [d_roll_vals] * d_rolls
+        combos = [list(tup) for tup in product(*iters)]
+
+        # Match highest rolls and determine losses.
+        n = len(combos)
+        total_losses = min(a_rolls, d_rolls)
+        for combo in combos:
+            a_vals = sorted(combo[:a_rolls], reverse=True)
+            d_vals = sorted(combo[a_rolls:], reverse=True)
+            a_losses = sum([1 for a, d in zip(a_vals, d_vals) if a <= d])
+            loss_probs_rolls[(a_losses, total_losses - a_losses)] += 1 / n
+
+        # # Go from counts to probs. Do this after everything's already been
+        # # counted just to minimize floating point errors.
+        # loss_probs_rolls = {k: x / n for k, x in loss_probs_rolls.items()}
+
+        loss_probs[(a_rolls, d_rolls)] = loss_probs_rolls
+    return loss_probs
+
+
+def calc_battle_probs(a,
+                      d,
+                      a_sides=A_SIDES_DEFAULT,
+                      d_sides=D_SIDES_DEFAULT,
+                      stop=STOP_DEFAULT):
     """Get probability of all outcomes.
 
     Args:
-        a: Number of troops on attacking territory
-        d: Number of troops on defending territory
-        stop: When attack has this many troops or fewer, stop
+        a (int): Number of troops on attacking territory
+        d (int): Number of troops on defending territory
+        stop (int): When attack has this many troops or fewer, stop
 
     Returns:
         Dict where key is tuple of final attack and defense troops, and
         values are probabilites of those outcomes
     """
-    return calc_battle_probs_recur(a, d, stop, dict())
+    assert a > 1 and d > 0
+    assert stop > 0
+    assert a_sides > 0 and d_sides > 0
+    return calc_battle_probs_recur(a, d, a_sides, d_sides, stop, dict())
 
 
-def calc_battle_probs_recur(a, d, stop, memo):
+def calc_battle_probs_recur(a, d, a_sides, d_sides, stop, memo):
     """Recursive helper."""
     if a <= stop or d == 0:
         return {(a, d): 1}  # outcome has prob = 1 in absorbing state
@@ -56,14 +107,14 @@ def calc_battle_probs_recur(a, d, stop, memo):
         return memo[(a, d)]
 
     # Compute probabilities of next roll outcomes.
-    next_loss_probs = loss_probs[(min(3, a - 1), min(2, d))]
+    next_loss_probs = get_loss_probs(a_sides, d_sides)[(min(3, a - 1), min(2, d))]
     probs_list = list()
     for (a_loss, d_loss), p2 in next_loss_probs.items():
         a2, d2 = a - a_loss, d - d_loss  # new attack and defense counts
         if (a2, d2) in memo:
             new = memo[(a2, d2)]
         else:
-            new = calc_battle_probs_recur(a2, d2, stop, memo)
+            new = calc_battle_probs_recur(a2, d2, a_sides, d_sides, stop, memo)
         # Multiply by upstream probability.
         new = {(a, d): p * p2 for (a, d), p in new.items()}
         probs_list.append(new)
@@ -109,14 +160,25 @@ def main():
     parser.add_argument('d',
                         type=int,
                         help='number of troops on defending territory')
+    parser.add_argument('--asides',
+                        default=A_SIDES_DEFAULT,
+                        type=int,
+                        help='number of sides on attack dice')
+    parser.add_argument('--dsides',
+                        default=D_SIDES_DEFAULT,
+                        type=int,
+                        help='number of sides on defense dice')
     parser.add_argument('--stop',
                         default=STOP_DEFAULT,
                         type=int,
                         help='when attack has this many troops or fewer, stop')
     args = parser.parse_args()
-    assert args.stop > 0
 
-    battle_probs = calc_battle_probs(args.a, args.d, args.stop)
+    battle_probs = calc_battle_probs(args.a,
+                                     args.d,
+                                     args.asides,
+                                     args.dsides,
+                                     args.stop)
     win_prob = calc_win_prob(battle_probs)
 
     # Print all probabilities.
